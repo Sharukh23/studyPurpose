ef7b05882b1e26751482c365d9d556326ae96aac
sharuk <sharukhahamed23@gmail.com>
2023-03-21 16:58:07 +0530

    changedtitle

--- prnt.java
+++ prnt.java
@@ -1,5 +1,9 @@
 class prnt{
     public static void main(String[] args) {
-        System.out.println("Hell");
+        System.out.println("Hellow");
+        System.out.println("How are you");
+        System.out.println("where are you");
+        System.out.println("iam fine okay");
+        System.out.println("cool");
     }
 } 
